321816d6fd8d4d040500af6603621133e509ea1a
fathannnur <carelius02@gmail.com>
2026-05-05 19:30:08 +0700

    refactor: remove redundant class definition and main function code

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -1,24 +1,2 @@
 #include <iostream>
 using namespace std;
-class buku{
-    string judul;
-
-    public:
-    buku &setJudul(string j){
-        this->judul = j;
-        return *this;
-    }
-    string getJudul(){
-        return this->judul;
-    }
-
-};
-
-int main()
-{
-    buku bukunya;
-    // bukunya.setJudul("Matematika");
-    // cout << bukunya.getJudul();
-    cout<<bukunya.setJudul("Matematika").getJudul();
-    return 0;   
-}
